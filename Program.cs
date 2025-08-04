f0be332b48136ff000017bb3bf64524e1c673f8b
Nemanja1208 <nevaljao994@gmail.com>
2025-08-04 23:02:48 +0200

    program.cs implementation av ARV

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@
             myAccount.Deposit(1000);
             myAccount.Withdraw(250);
             myAccount.PrintBalance(); // Output: Account Holder: Anna Andersson, Balance: 750 kr
+
+            // ARV exempel
+            SavingsAccount mySavings = new SavingsAccount();
+            mySavings.AccountHolder = "Alex Andersson";
+            mySavings.InterestRate = 0.05m; // 5% ränta
+
+            mySavings.Deposit(1000);
+            mySavings.ApplyInterest();
+            mySavings.PrintBalance(); // Saldo bör nu vara 1050 kr
         }
     }
 }
