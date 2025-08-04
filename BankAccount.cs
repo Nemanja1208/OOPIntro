494ffe65d4068ed7a571a152e7cdfb516912c1a7
Nemanja1208 <nevaljao994@gmail.com>
2025-08-04 22:57:48 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace OOPIntro
+{
+    public class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Account Holder: {AccountHolder}, Balance: {Balance} kr");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPIntro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount();
+            myAccount.AccountHolder = "Anna Andersson";
+            myAccount.Deposit(1000);
+            myAccount.Withdraw(250);
+            myAccount.PrintBalance(); // Output: Account Holder: Anna Andersson, Balance: 750 kr
+        }
+    }
+}
